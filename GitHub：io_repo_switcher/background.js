a305a86b001ff69441ad56e27f592073b6ed69d9
ManeNeko <sucarnell@gmail.com>
2026-05-04 08:30:15 +0900

    Add files via upload

--- GitHub：io_repo_switcher/background.js
+++ GitHub：io_repo_switcher/background.js
@@ -9,7 +9,7 @@ chrome.runtime.onInstalled.addListener(() => {
 });
 
 // メイン処理
-chrome.contextMenus.onClicked.addListener((info, tab) => {
+chrome.contextMenus.onClicked.addListener(async (info, tab) => {
 	if (!tab?.url) return;
 
 	// URL分解
@@ -17,19 +17,32 @@ chrome.contextMenus.onClicked.addListener((info, tab) => {
 	const host = url.hostname;
 	const paths = url.pathname.split('/').filter(Boolean);
 
+	let targetUrl = null;
+
 	// Pages → Repository
 	if (host.endsWith('.github.io')) {
 		const user = host.split('.')[0];
 		const repo = paths[0] || `${user}.github.io`;
-		chrome.tabs.update(tab.id, { url: `https://github.com/${user}/${repo}` });
+		targetUrl = `https://github.com/${user}/${repo}`;
 	}
 
 	// Repository → Pages
 	else if (host === 'github.com' && paths.length >= 2) {
-		const user = paths[0];
-		const repo = paths[1];
-		chrome.tabs.update(tab.id, {
-			url: `https://${user}.github.io/${repo === `${user}.github.io` ? '' : repo + '/'}`
+		const [user, repo] = paths;
+		targetUrl = `https://${user}.github.io/${repo === `${user}.github.io` ? '' : repo + '/'}`;
+	}
+
+	if (!targetUrl) return;
+
+	// URLの存在確認
+	const exists = await fetch(targetUrl, { method: 'HEAD' }).then(r => r.ok).catch(() => false);
+
+	if (exists) {
+		chrome.tabs.update(tab.id, { url: targetUrl });
+	} else {
+		chrome.scripting.executeScript({
+			target: { tabId: tab.id },
+			func: () => alert("ページが存在しません。")
 		});
 	}
 });
